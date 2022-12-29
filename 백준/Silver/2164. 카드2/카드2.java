943ed69b9c4ccd77d4481aad1d135f27d030730e
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-29 20:44:35 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 45564 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 1; i <= t; i++) {
+            q.add(i);
+        }
+        while(q.size() !=1) {
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.poll());
+
+    }
+}
