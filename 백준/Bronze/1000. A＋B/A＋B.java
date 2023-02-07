a69438b87b5433c8599f84439fc8b77272f62ee2
woojcoding <woojcoding@gmail.com>
2023-02-07 21:02:09 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+        bw.write(String.valueOf(a+b));
+        bw.flush();
+        bw.close();
+    }
+}
