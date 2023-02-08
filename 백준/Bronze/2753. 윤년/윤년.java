163dbd6d8ea22aa638270a44b920308822935488
woojcoding <woojcoding@gmail.com>
2023-02-08 21:58:49 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder();
+
+        int year = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            sb.append(1);
+        } else {
+            sb.append(0);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
