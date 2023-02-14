ced2e2d537d3d0ce0a305125289842a03d0e42cd
JungWoo Kim <woojcoding@gmail.com>
2023-02-14 21:02:43 +0900

    [Bronze IV] Title: 숫자의 합, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+
+        int result = 0;
+
+        for (byte num : br.readLine().getBytes()) {
+            result += num - '0';
+        }
+
+        br.close();
+
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+    }
+}
