3d48908700035afe6d3d3e33cb8a0fd61fc829fe
JungWoo Kim <woojcoding@gmail.com>
2023-03-20 10:43:57 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,13 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2 - num1 + 1];
-        
-        int num = 0;
-        
-        for (int i = num1; i <= num2; i++) {
-            answer[num++] = numbers[i];
-        }
-        
-        return answer;
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
