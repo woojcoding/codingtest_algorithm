68f1dfb3fadfb2dff52d5e3731aed5e3445ebd1e
JungWoo Kim <woojcoding@gmail.com>
2023-03-20 11:07:31 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.45 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,15 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] * numbers[j] > answer) {
-                    answer = numbers[i] * numbers[j];
-                }
-            }
-        }
+        Arrays.sort(numbers);
         
-        return answer;
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
