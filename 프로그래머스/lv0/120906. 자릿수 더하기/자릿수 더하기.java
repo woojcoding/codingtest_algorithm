10bca76caa02e041de3112c0f1451a149bfa63a6
JungWoo Kim <woojcoding@gmail.com>
2023-03-21 09:25:27 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while (true) {
+            if (n == 0) {
+                break;
+            }
+            answer += n % 10;
+            
+            n /= 10;
+        }
+        
+        return answer;
+    }
+}
