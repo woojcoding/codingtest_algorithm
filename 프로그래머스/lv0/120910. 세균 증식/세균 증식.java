370f3b232b1221e9f85dce8853fd4ac6ca09c7c9
JungWoo Kim <woojcoding@gmail.com>
2023-03-21 20:23:42 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        
+        while (t-- > 0) {
+            answer *= 2;
+        }
+        
+        return answer;
+    }
+}
