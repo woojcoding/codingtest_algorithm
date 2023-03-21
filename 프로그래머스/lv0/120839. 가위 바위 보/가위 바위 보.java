7b53e710a9dd9c315b0e0da7756c0e01bb2db42e
JungWoo Kim <woojcoding@gmail.com>
2023-03-21 16:35:59 +0900

    [level 0] Title: 가위 바위 보, Time: 2.56 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for (int i = 0; i < rsp.length(); i++) {
+            char c = rsp.charAt(i);
+            
+            switch (c) {
+                case '2':
+                    answer += "0";
+                    break;
+                case '0':
+                    answer += "5";
+                    break;
+                case '5':
+                    answer += "2";
+                    break;    
+            }
+        }
+        
+        return answer;
+    }
+}
